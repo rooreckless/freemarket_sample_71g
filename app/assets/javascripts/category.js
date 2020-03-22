dcc06cc86b47116d261c8519c2606e5b0c7187c8
naoya00 <bsk470@gmail.com>
2020-03-22 15:39:18 +0900

    アンセストリー実装

---
+++ app/assets/javascripts/category.js
@@ -0,0 +1,83 @@
+$(function(){
+  // カテゴリーセレクトボックスのオプションを作成
+  function appendOption(category){
+    var html = `<option value="${category.name}" data-category="${category.id}">${category.name}</option>`;
+    return html;
+  }
+  // 子カテゴリーの表示作成
+  function appendChidrenBox(insertHTML){
+    var childSelectHtml = '';
+    childSelectHtml = `<div class='formBox__select' id='categorySelect2'>
+    <select class="listing-select-wrapper__box--select2" id='parent_category2' name="product[category]"><option value="---">---</option>
+    ${insertHTML}
+    </select>
+    </div>`;
+    $('#categorySelect1').after(childSelectHtml);
+  }
+
+  function appendGrandchidrenBox(insertHTML){
+    var grandchildSelectHtml = '';
+    grandchildSelectHtml = `<div class='formBox__select' id='categorySelect3'>
+    <select class="listing-select-wrapper__box--select3" id='parent_category3' name="product[category]"><option value="---">---</option>
+    ${insertHTML}
+    </select>
+    </div>`;
+    $('#categorySelect2').after(grandchildSelectHtml);
+  }
+
+  // 親カテゴリー選択後のイベント
+  $('#parent_category').on('change', function(){
+    var parentCategory = document.getElementById('parent_category').value; //選択された親カテゴリーの名前を取得
+    if (parentCategory != "---"){ //親カテゴリーが初期値でないことを確認
+      $.ajax({
+        url: 'get_category_children',
+        type: 'GET',
+        data: { parent_name: parentCategory },
+        dataType: 'json'
+      })
+      .done(function(children){
+        $('#categorySelect2').remove();
+        $('#categorySelect3').remove();
+        var insertHTML = '';
+        children.forEach(function(child){
+          insertHTML += appendOption(child);
+        });
+        appendChidrenBox(insertHTML);
+      })
+      .fail(function(){
+        alert('カテゴリー取得に失敗しました');
+      })
+    }else{
+      $('#categorySelect2').remove();
+      $('#categorySelect3').remove();
+    }
+  });
+
+
+  $('#categorySelect__box').on('change', '#parent_category2', function(){
+    var childId = $('#parent_category2 option:selected').data('category'); //選択された子カテゴリーのidを取得
+    if (childId != "---"){ //子カテゴリーが初期値でないことを確認
+      $.ajax({
+        url: 'get_category_grandchildren',
+        type: 'GET',
+        data: { child_id: childId },
+        dataType: 'json'
+      })
+      .done(function(grandchildren){
+        if (grandchildren.length != 0) {
+          $('#categorySelect3').remove();
+          var insertHTML = '';
+          grandchildren.forEach(function(grandchild){
+            insertHTML += appendOption(grandchild);
+          });
+          appendGrandchidrenBox(insertHTML);
+        }
+      })
+      .fail(function(){
+        alert('カテゴリー取得に失敗しました');
+      })
+    }else{
+      $('#categorySelect3').remove();
+    }
+  });
+});
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,10 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
     @product.build_image
+    @category_parent_array = ["---"]
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+    end
   end
 
   def create
@@ -16,6 +20,14 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def get_category_children
+    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+ end
+
+ def get_category_grandchildren
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
+ end
 
   private
   def product_params 
