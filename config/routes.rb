76d0f9a4ca24dec396d933d5d1d4cff260d0f257
Kojiro-st <tenmadetobe5a@gmail.com>
2020-03-23 12:34:39 +0900

    1st

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "products#index"
   #上の記述はデプロイができている確認用のビューを表示するためのものです。確認出来次第削除願います。
   #また、デプロイが確認されたあとは、deploytestコントローラと、views内のdeploytestディレクトリも削除願います。
-  resources :products, only: [:new, :show, :create, :index] do
+  resources :products, only: [:new, :show, :create, :index, :edit] do
     collection do
       get 'get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', defaults: { format: 'json' }
