53c3ecc4a018419414798da6a906fa8cd40f2601
MaruyamaNao <61174627+MaruyamaNao@users.noreply.github.com>
2020-03-25 22:14:45 +0900

    save

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,8 +25,6 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-    @product_nikname = Product.includes(:users)
-
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
+  before_action :move_index, only: [:index]
 
   def index
   end
+
   def show 
+  end
+
+  private
+    def move_index
+        redirect_to controller: :products, action: :index
+      unless current_user.id == @uses.saler.id
+    end
   end
 end
