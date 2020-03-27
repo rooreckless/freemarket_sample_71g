18d3d3a3a44ca66644019416483112d44b03008b
nobu72 <nobu7238@gmail.com>
2020-03-27 20:33:23 +0900

    userコントローラーindex変更修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
 
 
   def index
-    @user = User.find(params[:id]) 
-    @user = User.where(user_id: current_user.id)
+    # @user = User.find(params[:id]) 
+    # @user = User.where(user_id: current_user.id)
   end
 
   def show
