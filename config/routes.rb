9d0809b6a13a2bc078dda4b77991c1f7f8b1cbc5
Kojiro-st <tenmadetobe5a@gmail.com>
2020-03-27 16:36:23 +0900

    下から四つ修正完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'purchase/index'
-  get 'purchase/done'
-  get 'card/new'
-  get 'card/show'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
@@ -19,7 +15,6 @@ Rails.application.routes.draw do
   end
   resources :card, only: [:new, :show] do
     collection do
-      post 'show', to: 'card#show'
       post 'pay', to: 'card#pay'
       post 'delete', to: 'card#delete'
     end
