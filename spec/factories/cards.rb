bb1dda74ccbf24973528acd0df5766417ea8ca9f
Kojiro-st <tenmadetobe5a@gmail.com>
2020-03-27 15:01:52 +0900

    単体テスト途中

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :card do
-    user_id { 1 }
-    buyer_id { "MyString" }
-    card_id { "MyString" }
+    user_id           {"1"}
+    customer_id       {"1"}
+    card_id           {"1"}
   end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,35 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+# RSpec.describe Card, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+require 'rails_helper'
+describe Card do
+  describe '#create' do
+
+    context 'log in' do
+      before do
+        login user
+      end
+
+      it "user_idが空では登録できない" do
+        card = build(:card, user_id: "")
+        card.valid?
+        expect(card.errors[:user_id]).to include("を入力してください")
+      end
+
+      it "customer_idが空では登録できない" do
+        card = build(:card, customer_id: "")
+        card.valid?
+        expect(card.errors[:customer_id]).to include("を入力してください")
+      end
+
+      it "card_idが空では登録できない" do
+        card = build(:card, card_id: "")
+        card.valid?
+        expect(card.errors[:card_id]).to include("を入力してください")
+      end
+    end
+  end
 end
