c63005cf2f44059ade5b5811110474d4de15ca2c
足立守 <rooreckless@gmail.com>
2020-03-29 11:59:48 +0900

    payjp 本番環境デバッグ用 表示を追加

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -8,20 +8,31 @@ class CardController < ApplicationController
   end
 
   def pay 
-    
+    puts "card_pay--1"
+    logger.info 'card_pay--1'
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+    puts "card_pay--2"
+    logger.info 'card_pay--2'
     if params['payjp-token'].blank?
       redirect_to action: "new"
     else
+      puts "card_pay--3"
+      logger.info 'card_pay--3'
       customer = Payjp::Customer.create(
       card: params['payjp-token'],
       metadata: {user_id: current_user.id}
       ) #念の為metadataにuser_idを入れましたがなくてもOK
+      puts "card_pay--4"
+      logger.info 'card_pay--4'
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
+        puts "card_pay--5"
+        logger.info 'card_pay--5'
         # redirect_to action: "show"
         redirect_to card_path(@card.id)
       else
+        puts "card_pay--6"
+      logger.info 'card_pay--6'
         # カード登録の際にエラーが出たら、再度、登録画面に戻し増す。
         redirect_to new_card_path, notice: 'カード登録時にエラーが発生しました。'
 
--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -21,26 +21,37 @@ class PurchaseController < ApplicationController
     # params.require(:product_id)はviews/purchase/show.html.hamlの「form_with url: pay_purchase_index_path」のf.hiddenfieldからくる値=params.require(:product_id)=購入した商品のid
     # この「購入した商品のid」でテーブルを検索します。
     purchased_product=Product.find(params.require(:product_id))
-
+    # TODO:本番環境はここでエラー
     card = Card.where(user_id: current_user.id).first
+    puts "purchase_pay--1"
+    logger.info 'purchase_pay--1'
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+    puts "purchase_pay--2"
+    logger.info 'purchase_pay--2'
     Payjp::Charge.create(
     amount: purchased_product.price, #支払金額を入力（購入した商品を元に、テーブルの値をとってきます。）
     customer: card.customer_id, #顧客ID
     currency: 'jpy', #日本円
   )
-
+  puts "purchase_pay--3"
+  logger.info 'purchase_pay--3'
   #購入後のデータベース書き込み処理 
   
   # 検索結果の「購入した商品」のbuyer_idカラムの値を、「購入したときのcardのuser_id」でセットします。
   purchased_product.buyer_id=card.user_id
+  puts "purchase_pay--4"
+  logger.info 'purchase_pay--4'
   # 「購入した商品」を更新するためsaveしますが、
   if purchased_product.save
     #saveに成功すれば、完了画面へ
     # redirect_to action: 'done' #完了画面に移動
+    puts "purchase_pay--5"
+    logger.info 'purchase_pay--5'
     redirect_to product_path(params.require(:product_id)), notice: '購入完了しました。'
     # saveに失敗したらflashを表示し、商品詳細画面に戻ります。
   else
+    puts "purchase_pay--6"
+    logger.info 'purchase_pay--6'
     redirect_to product_path(params.require(:product_id)), notice: 'エラーが発生しました。'
   end
   
