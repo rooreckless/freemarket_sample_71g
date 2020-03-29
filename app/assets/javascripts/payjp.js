fd1bbe0a802a0c2a878087264e9929a436d2d726
足立守 <rooreckless@gmail.com>
2020-03-29 21:55:06 +0900

    Revert "payjp本番環境 デバッグ payjp.jsを現在は使用していない状態でも購入できるかテスト"
    This reverts commit b579f65f61b2a4404bf5a139337566b9e46e56b8.
    
    前回の内容ではエラーが出たと思われるため、commitをrevertします。

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -1,5 +1,3 @@
-/*
-//もしかしたらこのjsファイルは実質使っていないのか確かめるため、全部コメントアウトしてみます。
 document.addEventListener(
   "DOMContentLoaded", e => {
     if (document.getElementById("token_submit") != null) { //token_submitというidがnullの場合、下記コードを実行しない
@@ -33,4 +31,3 @@ document.addEventListener(
   },
   false
 );
-*/
